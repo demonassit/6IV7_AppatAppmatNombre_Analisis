262d5db902b452b38b8c5231b6ac038a2dc068f6
demonassit <jminorg@ipn.mx>
2025-03-10 17:34:29 -0600

    estadistica

---
+++ EstaditicaDescriptiva/02Diccinario.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+#crear una funcion que se encargue de recibir un diccionario de las notas de los estudiantes de analisis de datos que van a reprobar y obtener su min, max, media, y desviasion estandar
+
+def estadisticas_notas(notas):
+    notas = pd.Series(notas)
+    estadistica = pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index=['Min', 'Max', 'Media', 'Desviasion Estandar'])
+    return estadistica
+
+notas = {'Juan':9, 'Juanita':5.9, 'Pedro':8.2, 'Rosalba':6.9, 'Federico':4.5, 'Alberto':7.5 }
+
+print(estadisticas_notas(notas))
