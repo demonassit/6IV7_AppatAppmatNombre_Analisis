c098e1872c9c9490425c9fab6fffefa1de2e5e3b
demonassit <jminorg@ipn.mx>
2025-03-10 17:42:20 -0600

    rango con notas

--- EstaditicaDescriptiva/02Diccinario.py
+++ EstaditicaDescriptiva/02Diccinario.py
@@ -7,6 +7,11 @@ def estadisticas_notas(notas):
     estadistica = pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index=['Min', 'Max', 'Media', 'Desviasion Estandar'])
     return estadistica
 
+def aprobados(notas):
+    notas = pd.Series(notas)
+    return notas[notas >= 6].sort_values(ascending=False)
+
 notas = {'Juan':9, 'Juanita':5.9, 'Pedro':8.2, 'Rosalba':6.9, 'Federico':4.5, 'Alberto':7.5 }
 
+print(aprobados(notas))
 print(estadisticas_notas(notas))
